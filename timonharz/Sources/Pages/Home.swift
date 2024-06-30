7940d466917b5be5ace66dd0d598fa88c240f3bb
timonharz <timon.harz@icloud.com>
2024-06-30 21:42:33 +0200

    Changes

--- timonharz/Sources/Pages/Home.swift
+++ timonharz/Sources/Pages/Home.swift
@@ -5,13 +5,13 @@ struct Home: StaticPage {
     var title = "Home"
 
     func body(context: PublishingContext) async -> [BlockElement] {
-    
+    Group {
         Text("Timon Harz")
         .font(.title3)
         Spacer()
         
         Image("/images/mailicon.svg")
-        
+    }
 
     }
 }
